cccb40a3119d24c1b50baae13ba7a04b331e8338
Gautier Bureau <gautier.bureau@inria.fr>
2019-02-13 00:45:20 +0100

    added test code

---
+++ compile.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "You need to give build or clean as argument."
+  exit 1
+fi
+
+if [ "$1" = "build" ]; then
+  g++ -c -g -O0 foo.cpp -o foo.o
+  g++ -c -g -O0 main.cpp -o main.o
+  g++ foo.o main.o -o main
+  ./main
+elif [ "$1" = "build-tests" ]; then
+  g++ -c -g -O0 --coverage foo.cpp -o foo.o
+  g++ -c -g -O0 main.cpp -o main.o
+  g++ foo.o main.o --coverage -o main
+
+  g++ -c -g -O0 --coverage test.cpp -o test.o
+  g++ -g -O0 --coverage test.o foo.o -o unittest -lgtest -lgtest_main
+  ./unittest
+
+  # Lcov
+  # lcov --directory . --capture --output-file coverage.info.unittest
+  # lcov --extract coverage.info.unittest '*test-travis/foo*' --output-file coverage.info.unittest.filtered
+  # lcov --remove coverage.info.unittest '/usr*' '*test-travis/test*' --output-file coverage.info.unittest.filtered
+  # cat coverage.info.unittest.filtered >> coverage.info
+  # mkdir -p report-lcov
+  # genhtml --no-function-coverage -o report-lcov coverage.info
+
+  # Gcovr
+  # mkdir -p report-gcovr/gcov
+  # gcovr $PWD --root $PWD --html --html-details --output report-gcovr/index.html --keep --filter=.*foo.*
+  # mv *.gcov report-gcovr/gcov
+
+  # My way
+  mkdir -p coverage
+  for file in $(find . -name "*.gcno"); do
+    cpp_file_name=$(basename $file .gcno)
+    cpp_file=$(find . -name "$cpp_file_name.cpp") # In Cmake cpp is not need as it generates .cpp.gcno files
+    gcov -pb $cpp_file -o $file
+  done
+  rm -f \#usr\#*
+  mv *.gcov coverage
+  rm -f coverage/test*
+  #mkdir -p coverage/gcovr
+  #gcovr $PWD --root $PWD --html --html-details --use-gcov-files --object-directory coverage --output coverage/gcovr/index.html --keep --filter=.*foo.*
+elif [ "$1" = "clean" ]; then
+  rm -f *.o
+  rm -f *.gcno
+  rm -f *.gcda
+  rm -rf report-lcov
+  rm -rf report-gcovr
+  rm -rf coverage
+  rm -f coverage.info*
+  rm -f *.gcov
+  if [ -f "main" ]; then rm main; fi
+  if [ -f "unittest" ]; then rm unittest; fi
+else
+  echo "Nothing."
+fi
+
+# When compiling --coverage implies -fprofile-arcs -ftest-coverage
+# When linking --coverage implies -lgcov
